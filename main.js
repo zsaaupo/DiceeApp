b361972ae164dd896035dd7a16201c3dbc76e964
zsaaupo <zakariasarker407@gmail.com>
2023-04-11 17:42:54 +0600

    game completed

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-let randomNumber = Math.floor(Math.random() * 6 + 1)
-let loka = String(randomNumber)
-console.log(typeof(loka))
-document.querySelector(".img1").setAttribute("src", "images/dice"+(Math.floor(Math.random() * 6 + 1)).toString()+".png")
-document.querySelector(".img2").setAttribute("src", "images/dice"+(Math.floor(Math.random() * 6 + 1)).toString()+".png")
+let playerOne = Math.floor(Math.random() * 6 + 1)
+let playerTwo = Math.floor(Math.random() * 6 + 1)
+
+
+document.querySelector(".img1").setAttribute("src", "images/dice"+playerOne.toString()+".png")
+document.querySelector(".img2").setAttribute("src", "images/dice"+playerTwo.toString()+".png")
+
+
+if(playerOne == playerTwo){
+	document.getElementsByTagName("h1")[0].innerHTML = "Draw!";
+}
+
+if(playerOne > playerTwo){
+	document.getElementsByTagName("h1")[0].innerHTML = "player 1 wins";
+}
+
+if(playerOne < playerTwo){
+	document.getElementsByTagName("h1")[0].innerHTML = "player 2 wins";
+}
